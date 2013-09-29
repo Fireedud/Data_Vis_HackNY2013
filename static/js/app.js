8f9916ee380c928f67288f43674238d9d6a5de26
Jonathan Wilde <hello@jwilde.me>
2013-09-29 02:08:41 -0400

    Styles are finally getting not silly.

--- static/js/app.js
+++ static/js/app.js
@@ -33,16 +33,57 @@ App.TopicRoute = App.Route.extend({
 });
 
 App.MapView = Ember.View.extend({
-	classNames: ['map'],
+	layers: {},
+	weights: {},
+
+	_getFeatureKey: function (feature) {
+		return feature.properties.name;
+	},
+
+	_getFeatureStyle: function (weight) {
+		return {
+			weight: 2,
+			opacity: 1,
+			color: "white",
+			fillOpacity: 0.7,
+			fillColor: "blue"
+		};
+	},
+
+	stylize: function () {
+		for (var k in this.layers) {
+			if (!layers.hasOwnProperty(k))
+				continue;
+
+			var layer = this.layers[k];
+			var weight = this.weights[k] || 0;
+			layer.setStyle(this._getFeatureStyle(weight));
+		}
+	}.observes("weights"),
 
 	didInsertElement: function () {
 		var element = this.$().get(0);
-		var layer = new L.StamenTileLayer("toner-lite");
 		var map = new L.Map(element);
+		var tiles = new L.StamenTileLayer("toner-lite");
 
-		map.addLayer(layer);
-		map.setView([0, 0], 2);
+		map.setView([40, 0], 2);
+		map.addLayer(tiles);
 
-		this.set("map", map);
+		Ember.$.getJSON('/static/data/world.json')
+			.then(function(world) { 
+				L.geoJson(world, {
+					style: function (feature) {
+						var weight = this.weights[this._getFeatureKey(feature)] || 0;
+						return this._getFeatureStyle(weight);
+					}.bind(this),
+					onEachFeature: function (feature, layer) {
+						this.layers[this._getFeatureKey(feature)] = layer;
+					}.bind(this)
+				}).addTo(map); 
+			}.bind(this));
 	}
+});
+
+App.SliderView = Ember.View.extend({
+
 });
