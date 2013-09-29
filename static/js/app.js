a2a2b95c646784ab053ac314d75b614135cd4a74
Jonathan Wilde <hello@jwilde.me>
2013-09-29 00:05:47 -0400

    Tweaking wording.

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,11 @@
 var App = Ember.Application.create();
 
 App.TopicModel = Ember.Object.extend({
-	search: ""
+	topic: ""
 });
 
 App.Router.map(function () {
-	this.resource('topic', { path: '/topic/:search' });
+	this.resource('topic', { path: '/topic/:topic' });
 });
 
 App.Route = Ember.Route.extend({
@@ -13,7 +13,7 @@ App.Route = Ember.Route.extend({
 		goToTopic: function (topic) {
 			// TODO: Display some sort of spinner that denotes whether there 
 			// were any articles found for the topic.
-			var model = App.TopicModel.create({ search: topic });
+			var model = App.TopicModel.create({ topic: topic });
 			this.transitionTo("topic", model);
 		}
 	}
@@ -23,12 +23,12 @@ App.IndexRoute = App.Route.extend({});
 App.TopicRoute = App.Route.extend({
 	model: function (params) {
 		// TODO: Actually fetch data from the server in this condition.
-		var model = App.TopicModel.create({ search: params.search });
+		var model = App.TopicModel.create({ topic: params.topic });
 		return model;
 	},
 
 	serialize: function (model, params) {
-		return { search: model.get("search") }
+		return { topic: model.get("topic") }
 	} 
 });
 
@@ -39,7 +39,11 @@ App.MapView = Ember.View.extend({
 		var element = this.$().get(0);
 		var layer = new L.StamenTileLayer("toner-lite");
 		var map = new L.Map(element);
+
 		map.addLayer(layer);
 		map.setView([0, 0], 2);
+
+		this.set("map", map);
+		this.set("countries", )
 	}
 });
